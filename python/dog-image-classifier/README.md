0dd7f2b39f5a44f9daea95b8e4ed29703e07ebd5
sarincasm <eelsweb@gmail.com>
2021-01-15 07:58:06 +0530

    python: image classifier - labels, classify

--- python/dog-image-classifier/README.md
+++ python/dog-image-classifier/README.md
@@ -6,6 +6,20 @@ Using Python to classify images of dogs & their breeds.
 
 Code based on [this lab](https://github.com/udacity/AIPND/tree/master/intropylab-classifying-images) from AI Programming with Python (Udacity).
 
+# Setup
+
+-  Checkout & clone the repo
+-  Install packages
+-  Download data from [here](https://go.g6g.me/pdoimclpi)
+
+Run
+
+```
+python check_images.py
+```
+
+Run with --help to see what arguments can be passed.
+
 # Useful Links
 
 -  https://github.com/udacity/AIPND/tree/master/intropylab-classifying-images
--- python/dog-image-classifier/check_images.py
+++ python/dog-image-classifier/check_images.py
@@ -1,5 +1,21 @@
-from time import time, sleep
+from os import listdir, path
+from time import time
 import argparse
+
+from pretrained.classifier import classifier
+
+
+def main():
+    start_time = time()
+    input_args = get_input_args()
+    true_labels = get_pet_labels(input_args.image_dir)
+    result = classify_images(input_args.image_dir,
+                             true_labels, input_args.arch)
+    print(result)
+    end_time = time()
+
+    print('\n')
+    print('Running Time: ', get_formatted_running_time(start_time, end_time))
 
 
 def get_formatted_running_time(start_time, end_time):
@@ -19,7 +35,8 @@ def get_input_args():
         parse_args()
     """
     parser = argparse.ArgumentParser(description='add description here')
-    parser.add_argument('--image-dir', type=str, help='path to images folder')
+    parser.add_argument('--image-dir', type=str,
+                        help='path to images folder', default='pet_images')
     parser.add_argument('--arch', type=str, default='vgg',
                         choices=['vgg', 'resnet', 'alexnet'], help='CNN model architecture to use')
     parser.add_argument('--dogfile', type=str,
@@ -27,14 +44,79 @@ def get_input_args():
     return parser.parse_args()
 
 
-def main():
-    start_time = time()
-    input_args = get_input_args()
-    print(input_args)
-    sleep(1.6)
-    end_time = time()
+def get_pet_labels(image_dir):
+    """
+    This is used to check accuracy of the classifier.
+    Read pet filenames & extract label from filename.
+    Return it as a dict.
+    Parameters:
+        image_dir
+    Returns:
+        true_labels
+    """
+    filenames_list = listdir(image_dir)
+    true_labels = dict()
+    for filename in filenames_list:
+        fname_lower = filename.lower()
+        components = fname_lower.split('_')
+        pet_label = ''
+        for word in components:
+            if word.isalpha():
+                pet_label += word + ' '
+        pet_label = pet_label.strip()
+        if filename not in true_labels:
+            true_labels[filename] = pet_label
 
-    print('Running Time: ', get_formatted_running_time(start_time, end_time))
+    return true_labels
+
+
+def classify_images(image_dir, true_labels, model):
+    """
+    Parameters:
+        image_dir
+        true_labels
+        model
+    Returns:
+        result - dict {filename: list}
+            list in this form:
+            [
+                true_label,
+                classifier_label,
+                1/0 - match b/w true & classifier labels
+            ]
+    """
+    result = dict()
+    for filename in true_labels:
+        classifier_label = classifier(path.join(image_dir, filename), model)
+        classifier_label = classifier_label.lower()
+        classifier_label = classifier_label.strip()
+
+        true_label = true_labels[filename]
+
+        # classifier_label is sometimes a value like "a, b, c"
+        # where true_label might be "a" or "b" or "c"
+        # therefore we search if it actually matches
+        found = classifier_label.find(true_label)
+        if found >= 0:
+            if ((found == 0 and len(true_label) == len(classifier_label)) or
+                    (((found == 0) or (classifier_label[found - 1] == " ")) and
+                     ((found + len(true_label) == len(classifier_label)) or
+                              (classifier_label[found + len(true_label): found+len(true_label)+1] in
+                               (",", " "))
+                              )
+                     )
+                    ):
+                if filename not in result:
+                    result[filename] = [true_label, classifier_label, 1]
+            else:
+                if filename not in result:
+                    result[filename] = [true_label, classifier_label, 0]
+
+        else:
+            if filename not in result:
+                result[filename] = [true_label, classifier_label, 0]
+
+    return result
 
 
 if __name__ == "__main__":
