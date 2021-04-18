55bbee73299c6080b960dc97fd602f66e55bb82e
sarincasm <eelsweb@gmail.com>
2021-04-18 18:33:02 +0530

    configuring jest

--- reactiverse/jest/README.md
+++ reactiverse/jest/README.md
@@ -19,4 +19,5 @@ npm test
 ## Useful Links
 
 -  https://github.com/kentcdodds/jest-cypress-react-babel-webpack
+-  https://github.com/kentcdodds/jest-cypress-react-babel-webpack/tree/tjs/jest-23
 -  https://testingjavascript.com/
--- reactiverse/jest/jest.config.js
+++ reactiverse/jest/jest.config.js
@@ -1,21 +1,25 @@
 /** @format */
 
-const path = require('path')
-
 module.exports = {
-	testEnvironment: 'jest-environment-jsdom',
-	moduleDirectories: [
-		'node_modules',
-		path.join(__dirname, 'src'),
-		'shared',
-		path.join(__dirname, 'test'),
-	],
-	moduleNameMapper: {
-		// use these while resolving css, module css files
-		'\\.module\\.css$': 'identity-obj-proxy',
-		'\\.css$': require.resolve('./test/style-mock.js'),
+	...require('./test/jest-common'),
+	collectCoverageFrom: ['**/src/**/*.js'],
+	coverageThreshold: {
+		global: {
+			statements: 15,
+			branches: 10,
+			functions: 15,
+			lines: 15,
+		},
+		'./src/shared/utils.js': {
+			statements: 100,
+			branches: 80,
+			functions: 100,
+			lines: 100,
+		},
 	},
-	setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-	// custom snapshot serializer
-	snapshotSerializers: ['jest-emotion'],
+	projects: [
+		'./test/jest-lint.js',
+		'./test/jest-client.js',
+		'./test/jest-server.js',
+	],
 }
