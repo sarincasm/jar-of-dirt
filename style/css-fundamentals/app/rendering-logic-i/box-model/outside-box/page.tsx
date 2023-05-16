26670fb1eb8fc0cb874edbbbf25c80bca4fcedce
sarincasm <eelsweb@gmail.com>
2023-05-16 04:49:05 +0200

    CSS Fundamentals Rendering Logic: Outside the Box

--- style/css-fundamentals/app/rendering-logic-i/box-model/outside-box/page.tsx
+++ style/css-fundamentals/app/rendering-logic-i/box-model/outside-box/page.tsx
@@ -1,7 +1,7 @@
 function renderStarter() {
 	return (
 		<section className="p-4">
-			<div className="bg-white p-8 rounded-lg">
+			<div className="bg-white p-8 rounded-lg max-w-2xl mx-auto mt-4">
 				<h1 className="bg-pink-500 py-4 px-8 mb-6 text-center rounded text-3xl font-bold">
 					An Otter Essay
 				</h1>
@@ -19,8 +19,8 @@ function renderStarter() {
 function renderDone() {
 	return (
 		<section className="p-4">
-			<div className="bg-white p-8 rounded-lg">
-				<h1 className="bg-pink-500 py-4 px-8 mb-6 text-center rounded text-3xl font-bold">
+			<div className="bg-white p-8 rounded-lg max-w-2xl mx-auto mt-4">
+				<h1 className="bg-pink-500 py-4 px-8 mb-6 text-center rounded text-3xl font-bold -mt-12">
 					An Otter Essay
 				</h1>
 				<p>
