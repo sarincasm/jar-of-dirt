7de03be32b60536c10f3729e49a5d1a6e7747742
sarincasm <eelsweb@gmail.com>
2023-05-17 20:17:24 +0200

    CSS Fundamentals Flow Layout Figures : Done

--- style/css-fundamentals/app/rendering-logic-i/flow-layout/figures-captions/page.tsx
+++ style/css-fundamentals/app/rendering-logic-i/flow-layout/figures-captions/page.tsx
@@ -38,26 +38,26 @@ function renderDone() {
 	return (
 		<section className="p-4">
 			<div className="bg-white p-8 rounded-lg">
-				<figure className="p-2 border border-solid mb-8">
+				<figure className="p-2 border border-solid mb-8 w-min">
 					<Image
 						alt="A hallway with rainbow-colored lighting"
 						src="https://courses.joshwcomeau.com/cfj-mats/wall-art.jpg"
 						width={500}
 						height={750}
-						className="mb-2"
+						className="mb-2 max-w-[initial]"
 					/>
 					<figcaption className="text-center text-gray-600">
 						Photo by Efe Kurnaz in Camp Nou, Barcelona, Spain. Found on
 						Unsplash.
 					</figcaption>
 				</figure>
-				<figure className="p-2 border border-solid mb-8">
+				<figure className="p-2 border border-solid mb-8 w-min">
 					<Image
 						alt="A yawning kitten"
 						src="https://courses.joshwcomeau.com/cfj-mats/cat-avatar-250px.jpg"
 						width={250}
 						height={250}
-						className="mb-2"
+						className="mb-2 max-w-[initial]"
 					/>
 					<figcaption className="text-center text-gray-600">
 						Unknown photographer. Found on Unsplash.
