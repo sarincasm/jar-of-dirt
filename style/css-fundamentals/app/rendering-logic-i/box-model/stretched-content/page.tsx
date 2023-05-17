2aed0a2e9c70a8f7287253ee6031d3f97068855e
sarincasm <eelsweb@gmail.com>
2023-05-17 06:48:41 +0200

    CSS Fundamentals Rendering Logic: Stretched Content -  Done

--- style/css-fundamentals/app/rendering-logic-i/box-model/stretched-content/page.tsx
+++ style/css-fundamentals/app/rendering-logic-i/box-model/stretched-content/page.tsx
@@ -29,8 +29,8 @@ function renderStarter() {
 function renderDone() {
 	return (
 		<section className="p-4">
-			<div className="bg-white p-8 rounded-lg max-w-2xl mx-auto mt-4">
-				<p className="mb-4">
+			<div className="bg-white py-8 rounded-lg max-w-2xl mx-auto mt-4">
+				<p className="mb-4 px-8">
 					Otters have long, slim bodies and relatively short limbs. Their
 					most striking anatomical features are the powerful webbed feet
 					used to swim, and their seal-like abilities holding breath
@@ -43,7 +43,7 @@ function renderDone() {
 					alt="A cute otter in water"
 					src="https://courses.joshwcomeau.com/cfj-mats/otter.jpg"
 				/>
-				<p className="mb-4">
+				<p className="mb-4 px-8">
 					More importantly, otters are glorious water dogs, playful and
 					curious. The otter, no other, is the best animal.
 				</p>
