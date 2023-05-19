4c01d20933a7b80719dc2f2a8022cee86b31b0c6
sarincasm <eelsweb@gmail.com>
2023-05-19 18:39:37 +0200

    CSS Fundamentals Done: Bubble Border - Containing Blocks

--- style/css-fundamentals/app/rendering-logic-ii/containing-blocks/bubble-border/page.tsx
+++ style/css-fundamentals/app/rendering-logic-ii/containing-blocks/bubble-border/page.tsx
@@ -16,10 +16,10 @@ function renderDone() {
 	return (
 		<section className="p-4">
 			<div className="bg-white p-8 rounded-lg">
-				<div className="border-4 border-pink-500 h-36 m-16">
-					<div></div>
-					<div></div>
-					<div></div>
+				<div className="border-4 border-pink-500 h-36 m-16 relative">
+					<div className="w-16 h-16 rounded-full border-inherit border-4 absolute -left-8 -top-8 bg-white"></div>
+					<div className="w-8 h-8 rounded-full border-inherit border-4 absolute -left-4 top-9 bg-white"></div>
+					<div className="w-6 h-6 rounded-full border-inherit border-4 absolute left-9 -top-[12px] bg-white"></div>
 				</div>
 			</div>
 		</section>
