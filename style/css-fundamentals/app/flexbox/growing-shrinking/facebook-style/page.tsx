42b253d5562295f6ec5ea9742776111139b4e460
sarincasm <eelsweb@gmail.com>
2023-05-24 04:23:23 +0200

    css fundamentals - flexbox starter: growing and shrinking - facebook style layout

---
+++ style/css-fundamentals/app/flexbox/growing-shrinking/facebook-style/page.tsx
@@ -0,0 +1,36 @@
+function renderStarter() {
+	return (
+		<section className="p-4 bg-white flex">
+			<nav className="h-52 border-2 border-solid border-red-500 bg-red-500/20"></nav>
+			<main className="h-52 border-2 border-solid border-purple-400/20"></main>
+			<aside className="h-52 border-2 border-solid border-green-400/20 hidden md:block"></aside>
+		</section>
+	)
+}
+
+function renderDone() {
+	return (
+		<section className="p-4 bg-white flex">
+			<nav className="h-52 border-2 border-solid border-red-500 bg-red-500/20"></nav>
+			<main className="h-52 border-2 border-solid border-purple-400/20"></main>
+			<aside className="h-52 border-2 border-solid border-green-400/20 hidden md:block"></aside>
+		</section>
+	)
+}
+
+export default function Page() {
+	return (
+		<main className="bg-purple-500">
+			<div className="p-10">
+				<h2 className="text-2xl font-bold my-3 text-center text-white">
+					Starter
+				</h2>
+				{renderStarter()}
+				<h2 className="text-2xl font-bold my-3 text-center text-white">
+					Finished
+				</h2>
+				{renderDone()}
+			</div>
+		</main>
+	)
+}
