f157543788d7caf0a35d5e4fd6b553107769b265
sarincasm <eelsweb@gmail.com>
2023-07-22 23:41:13 +0200

    css fundamentals css variables - stacked cards

--- style/css-fundamentals/app/responsive/css-variables/stacked-cards/page.tsx
+++ style/css-fundamentals/app/responsive/css-variables/stacked-cards/page.tsx
@@ -2,13 +2,13 @@ function renderStarter() {
 	return (
 		<section className="py-8">
 			<main className="flex flex-col items-center gap-2 p-2 sm:gap-4 sm:p-4 md:gap-8 md:p-8">
-				<article className="p-2 rounded-lg sm:p-4 sm:rounded-2xl md:p-8 md:rounded-3x bg-white h-48 w-full max-w-xl flex justify-center">
+				<article className="p-2 rounded-lg sm:p-4 sm:rounded-2xl md:p-8 md:rounded-[32px] bg-white h-48 w-full max-w-xl flex justify-center">
 					Card 1
 				</article>
-				<article className="p-2 rounded-lg sm:p-4 sm:rounded-2xl md:p-8 md:rounded-3x bg-white h-48 w-full max-w-xl flex justify-center">
+				<article className="p-2 rounded-lg sm:p-4 sm:rounded-2xl md:p-8 md:rounded-[32px] bg-white h-48 w-full max-w-xl flex justify-center">
 					Card 2
 				</article>
-				<article className="p-2 rounded-lg sm:p-4 sm:rounded-2xl md:p-8 md:rounded-3x bg-white h-48 w-full max-w-xl flex justify-center">
+				<article className="p-2 rounded-lg sm:p-4 sm:rounded-2xl md:p-8 md:rounded-[32px] bg-white h-48 w-full max-w-xl flex justify-center">
 					Card 3
 				</article>
 			</main>
@@ -19,14 +19,14 @@ function renderStarter() {
 function renderDone() {
 	return (
 		<section className="py-8">
-			<main className="flex flex-col items-center gap-2 p-2 sm:gap-4 sm:p-4 md:gap-8 md:p-8">
-				<article className="p-2 rounded-lg sm:p-4 sm:rounded-2xl md:p-8 md:rounded-3x bg-white h-48 w-full max-w-xl flex justify-center">
+			<main className="[--spacing:8px] flex flex-col items-center gap-[--spacing] p-[--spacing] sm:[--spacing:16px] md:[--spacing:32px]">
+				<article className="p-[--spacing] rounded-[--spacing] bg-white h-48 w-full max-w-xl flex justify-center">
 					Card 1
 				</article>
-				<article className="p-2 rounded-lg sm:p-4 sm:rounded-2xl md:p-8 md:rounded-3x bg-white h-48 w-full max-w-xl flex justify-center">
+				<article className="p-[--spacing] rounded-[--spacing] bg-white h-48 w-full max-w-xl flex justify-center">
 					Card 2
 				</article>
-				<article className="p-2 rounded-lg sm:p-4 sm:rounded-2xl md:p-8 md:rounded-3x bg-white h-48 w-full max-w-xl flex justify-center">
+				<article className="p-[--spacing] rounded-[--spacing] bg-white h-48 w-full max-w-xl flex justify-center">
 					Card 3
 				</article>
 			</main>
