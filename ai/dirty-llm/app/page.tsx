ce977584ee95a9a34cd168147a53492974df987d
sarincasm <eelsweb@gmail.com>
2024-06-01 17:49:10 +0200

    ai: dirty llm bump nextjs

--- ai/dirty-llm/app/page.tsx
+++ ai/dirty-llm/app/page.tsx
@@ -52,7 +52,6 @@ export default function Chat() {
 							id="comment"
 							className="block w-full resize-none border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
 							placeholder="Add your comment..."
-							defaultValue={''}
 							value={input}
 							onChange={handleInputChange}
 						/>
