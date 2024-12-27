a81902be4d6e096227b27793a6e6c95ea29617e6
sarincasm <eelsweb@gmail.com>
2024-12-27 23:33:14 +0100

    tdd-fast-api: add product model and migration

--- python/tdd-api/tdd_api/models.py
+++ python/tdd-api/tdd_api/models.py
@@ -4,7 +4,14 @@ from sqlalchemy import (
     String,
     Boolean,
     UniqueConstraint,
+    text,
+    Text,
+    DateTime,
+    Enum,
+    ForeignKey,
+    func,
 )
+from sqlalchemy.dialects.postgresql import UUID
 
 from .db_connection import Base
 
@@ -24,4 +31,41 @@ class Category(Base):
     __table_args__ = (
         UniqueConstraint("name", "level", name="uq_category_name_level"),
         UniqueConstraint("slug", name="uq_category_slug"),
+    )
+
+
+class Product(Base):
+    __tablename__ = "product"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    pid = Column(
+        UUID(as_uuid=True),
+        nullable=False,
+        server_default=text("uuid_generate_v4()"),
+    )
+    name = Column(String(200), nullable=False)
+    slug = Column(String(220), nullable=False)
+    description = Column(Text, nullable=True)
+    is_digital = Column(Boolean, nullable=False, default=False, server_default="False")
+    created_at = Column(
+        DateTime, server_default=text("CURRENT_TIMESTAMP"), nullable=False
+    )
+    updated_at = Column(
+        DateTime,
+        server_default=text("CURRENT_TIMESTAMP"),
+        onupdate=func.now(),
+        nullable=False,
+    )
+    is_active = Column(Boolean, nullable=False, default=False, server_default="False")
+    stock_status = Column(
+        Enum("oos", "is", "obo", name="status_enum"),
+        nullable=False,
+        server_default="oos",
+    )
+    category_id = Column(Integer, ForeignKey("category.id"), nullable=False)
+
+    __table_args__ = (
+        UniqueConstraint("name", name="uq_product_name"),
+        UniqueConstraint("slug", name="uq_product_slug"),
+        UniqueConstraint("pid", name="uq_product_pid"),
     )
---
+++ python/tdd-api/tests/models/test_product.py
@@ -0,0 +1,65 @@
+from sqlalchemy import Boolean, DateTime, Enum, Integer, String, Text
+from sqlalchemy.dialects.postgresql import UUID
+
+
+def test_model_structure_table_exists(db_inspector):
+    assert db_inspector.has_table("product")
+
+
+def test_model_structure_column_data_types(db_inspector):
+    table = "product"
+    columns = {columns["name"]: columns for columns in db_inspector.get_columns(table)}
+
+    assert isinstance(columns["id"]["type"], Integer)
+    assert isinstance(columns["pid"]["type"], UUID)
+    assert isinstance(columns["name"]["type"], String)
+    assert isinstance(columns["slug"]["type"], String)
+    assert isinstance(columns["description"]["type"], Text)
+    assert isinstance(columns["is_digital"]["type"], Boolean)
+    assert isinstance(columns["created_at"]["type"], DateTime)
+    assert isinstance(columns["updated_at"]["type"], DateTime)
+    assert isinstance(columns["is_active"]["type"], Boolean)
+    assert isinstance(columns["stock_status"]["type"], Enum)
+    assert isinstance(columns["category_id"]["type"], Integer)
+
+
+def test_model_structure_nullable_constraints(db_inspector):
+    table = "product"
+    columns = db_inspector.get_columns(table)
+
+    expected_nullable = {
+        "id": False,
+        "pid": False,
+        "name": False,
+        "slug": False,
+        "description": True,
+        "is_digital": False,
+        "created_at": False,
+        "updated_at": False,
+        "is_active": False,
+        "stock_status": False,
+        "category_id": False,
+    }
+
+    for column in columns:
+        column_name = column["name"]
+        assert column["nullable"] == expected_nullable.get(
+            column_name
+        ), f"column '{column_name}' is not nullable as expected"
+
+
+def test_model_structure_column_lengths(db_inspector):
+    table = "product"
+    columns = {columns["name"]: columns for columns in db_inspector.get_columns(table)}
+
+    assert columns["name"]["type"].length == 200
+    assert columns["slug"]["type"].length == 220
+
+
+def test_model_structure_unique_constraints(db_inspector):
+    table = "product"
+    constraints = db_inspector.get_unique_constraints(table)
+
+    assert any(constraint["name"] == "uq_product_name" for constraint in constraints)
+    assert any(constraint["name"] == "uq_product_slug" for constraint in constraints)
+    assert any(constraint["name"] == "uq_product_pid" for constraint in constraints)
