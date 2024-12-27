77e432e783ebeebf9e40eeba65bca316b4d52dc7
sarincasm <eelsweb@gmail.com>
2024-12-27 20:43:06 +0100

    tdd-fast-api: add unique and nullable constraints to category model

--- python/tdd-api/tdd_api/models.py
+++ python/tdd-api/tdd_api/models.py
@@ -1,4 +1,10 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    UniqueConstraint,
+)
 
 from .db_connection import Base
 
@@ -7,8 +13,15 @@ class Category(Base):
     __tablename__ = "category"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(100))
-    slug = Column(String(120))
-    is_active = Column(Boolean)
-    level = Column(Integer)
-    parent_id = Column(Integer)
+    name = Column(String(100), nullable=False)
+    slug = Column(String(120), nullable=False)
+    is_active = Column(Boolean, nullable=False)
+    level = Column(Integer, nullable=False)
+    parent_id = Column(Integer, nullable=True)
+
+    #  alembic cannot auto detect - CheckConstraint & default server values
+
+    __table_args__ = (
+        UniqueConstraint("name", "level", name="uq_category_name_level"),
+        UniqueConstraint("slug", name="uq_category_slug"),
+    )
--- python/tdd-api/tests/models/test_category.py
+++ python/tdd-api/tests/models/test_category.py
@@ -21,3 +21,41 @@ def test_model_structure_table_column_types(db_inspector):
     assert isinstance(columns["is_active"]["type"], Boolean)
     assert isinstance(columns["level"]["type"], Integer)
     assert isinstance(columns["parent_id"]["type"], Integer)
+
+
+def test_model_structure_nullable_constraints(db_inspector):
+    table_name = "category"
+    columns = db_inspector.get_columns(table_name)
+
+    expected_nullable = {
+        "id": False,
+        "name": False,
+        "slug": False,
+        "is_active": False,
+        "level": False,
+        "parent_id": True,
+    }
+
+    for column in columns:
+        column_name = column["name"]
+        assert column["nullable"] == expected_nullable.get(
+            column_name
+        ), f"column '{column_name}' is not nullable as expected"
+
+
+def test_model_structure_unique_constraints(db_inspector):
+    table_name = "category"
+    constraints = db_inspector.get_unique_constraints(table_name)
+
+    assert any(
+        constraint["name"] == "uq_category_name_level" for constraint in constraints
+    )
+    assert any(constraint["name"] == "uq_category_slug" for constraint in constraints)
+
+
+def test_model_structure_column_lengths(db_inspector):
+    table = "category"
+    columns = {columns["name"]: columns for columns in db_inspector.get_columns(table)}
+
+    assert columns["name"]["type"].length == 100
+    assert columns["slug"]["type"].length == 120
